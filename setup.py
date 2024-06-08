1fde79c1975d6b5a4ffae8e7eea3993d924191ac
Aarsh01 <95579399+Aarsh01@users.noreply.github.com>
2024-06-08 13:15:36 +0530

    latest updated

--- setup.py
+++ setup.py
@@ -5,11 +5,23 @@ from setuptools import find_packages,setup
 from typing import List
 # Later ....
 
+# from pip.req import parse_requirements
 
-def get_requirements()-> List[str]:
-    requirements_list=list[str]=[]
+def get_requirements() -> list[str]:
+    requirements_list: list[str] = []
     return requirements_list
 
+
+# install_reqs = parse_requirements('requirements.txt')
+
+# reqs = [str(ir.req) for ir in install_reqs]
+
+
+# import os
+# from setuptools import setup
+
+# with open('requirements.txt') as f:
+#     required = f.read().splitlines()
 
 setup(      
     name='sensor',
@@ -17,7 +29,7 @@ setup(
     author='aarsh',
     author_email='aarshmehtani01@gmail.com',
     packages=find_packages(),
-    install_requires= get_requirements(), #['pymongo'] # later we will make function {get_requirements()} so that we will not manually list the requirement.text libraies 
+    install_requires=get_requirements(), #['pymongo'] # later we will make function {get_requirements()} so that we will not manually list the requirement.text libraies 
     
     
 )
